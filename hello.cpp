dddb67a9d6eecfcf182f20c742e2764663b09f6e
Combe-Martin <Combe.Martin.43@gmail.com>
2014-06-16 21:09:30 +0200

    change output to Hello: World or Hello: argv[1]

--- hello.cpp
+++ hello.cpp
@@ -4,11 +4,11 @@ using namespace std ;
 
 int main(int argc,char **argv)
 {
-   string Hello="Combe-Martin" ;
+   string World = "World" ;
    if(argc>1)
-      Hello = argv[1] ;
+      World = argv[1] ;
  
-   cout << Hello << endl;
+   cout << "Hello: " << World << endl;
 
    return 0 ;
 }
